f32c9e8248e00cccfdc75a4980ccc4956c784c48
nayan.savi <nayan.savi@gmail.com>
2020-08-01 14:14:36 +0530

    Updates

---
+++ precache-manifest.1767ceba260faa6a403b52ee5def89af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39d368b7d52100f659bb34f61a9dc96b",
+    "url": "/react-course-app/index.html"
+  },
+  {
+    "revision": "c973b890f0be8737de37",
+    "url": "/react-course-app/static/css/2.e7c4c567.chunk.css"
+  },
+  {
+    "revision": "fe6ab0c4158b4b90dae7",
+    "url": "/react-course-app/static/css/main.97cb9e6c.chunk.css"
+  },
+  {
+    "revision": "c973b890f0be8737de37",
+    "url": "/react-course-app/static/js/2.2d81da9d.chunk.js"
+  },
+  {
+    "revision": "885c0e205ef5e79f4aef448a818ba6a1",
+    "url": "/react-course-app/static/js/2.2d81da9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe6ab0c4158b4b90dae7",
+    "url": "/react-course-app/static/js/main.d2c14474.chunk.js"
+  },
+  {
+    "revision": "bb48857f7c17e0af8655",
+    "url": "/react-course-app/static/js/runtime-main.4ce906c8.js"
+  },
+  {
+    "revision": "01b09eabf25a7c3f7f87d6b46f9cc322",
+    "url": "/react-course-app/static/media/TutotPoint_logo.01b09eab.png"
+  }
+]);
