6eea3931e8a5a2dfc59a639df66d082d642fce79
nayan.savi <nayan.savi@gmail.com>
2020-08-01 17:19:39 +0530

    form

--- src/components/common/CourseForm.js
+++ src/components/common/CourseForm.js
@@ -7,7 +7,7 @@ const CourseForm = (props) => {
 
     return (
         <Formik initialValues={props.initialValues} validationSchema={props.validationSchema} onSubmit={props.onSubmit}>
-            <form>
+            <Form>
                 <div className="form-group">
                     <label htmlFor="courseName" className="float-left">Course Name</label>
                     <Field name="courseName" component="select" className="form-control">
@@ -46,7 +46,7 @@ const CourseForm = (props) => {
                 </div>
 
                 <ToastContainer/>
-            </form>
+            </Form>
         </Formik>
     );
 };
