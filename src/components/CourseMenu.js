cba7f74d6b55b62619e4379f5bb45b729e8e03e7
nayan.savi <nayan.savi@gmail.com>
2020-08-01 15:23:35 +0530

    router issues

--- src/components/CourseMenu.js
+++ src/components/CourseMenu.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {NavDropdown, Nav, Navbar} from 'react-bootstrap';
+import {Route} from "react-router";
 
 const CourseMenu = () => {
     return (
         <Navbar bg="primary" variant="dark">
             <Nav className="mr-auto">
-                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href={process.env.PUBLIC_URL+ '/'} >Home</Nav.Link>
                 <NavDropdown title="Course" id="basic-nav-dropdown">
                     <NavDropdown.Item href={process.env.PUBLIC_URL+ '/addCourse'}>Add Course</NavDropdown.Item>
                     <NavDropdown.Item href={process.env.PUBLIC_URL+'/displayCourse'}>Display Course</NavDropdown.Item>
